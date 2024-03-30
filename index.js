9d121424547ddc44d5723ab1ceb961d962988cf0
Karmvir <jadejakarmvir12@gmail.com>
2024-03-30 09:05:34 +0530

    Covered the shift between the users and adding new user also covered some of the edge cases

--- index.js
+++ index.js
@@ -25,8 +25,10 @@ let users = [
 ];
 
 async function checkVisisted() {
-  const result = await db.query("SELECT country_code FROM visited_countries JOIN users ON users.id = users.id WHERE  user_id=$1;",
-  [currentUserId]);
+  const result = await db.query(
+    "SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1; ",
+    [currentUserId]
+  );
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
@@ -34,28 +36,26 @@ async function checkVisisted() {
   return countries;
 }
 
-
-async function getCurrentUser(){
+async function getCurrentUser() {
   const result = await db.query("SELECT * FROM users");
-  users =result.rows;
-  return users.find((user)=> user.id == currentUserId);
+  users = result.rows;
+  return users.find((user) => user.id == currentUserId);
 }
-
-
-
 
 app.get("/", async (req, res) => {
   const countries = await checkVisisted();
+  const currentUser = await getCurrentUser();
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
     users: users,
-    color: currentUserId.color,
+    color: currentUser.color,
   });
 });
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
   const currentUser = await getCurrentUser();
+
   try {
     const result = await db.query(
       "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
@@ -66,8 +66,8 @@ app.post("/add", async (req, res) => {
     const countryCode = data.country_code;
     try {
       await db.query(
-        "INSERT INTO visited_countries (country_code,user_id) VALUES ($1,$2);",
-        [countryCode,currentUserId]
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+        [countryCode, currentUserId]
       );
       res.redirect("/");
     } catch (err) {
@@ -77,21 +77,23 @@ app.post("/add", async (req, res) => {
     console.log(err);
   }
 });
+
 app.post("/user", async (req, res) => {
-    if(req.body.add ==="new"){
-      res.render("new.ejs")
-    }
-    else {
-      currentUserId = req.body.user;
-      res.redirect("/");
-    }
+  if (req.body.add === "new") {
+    res.render("new.ejs");
+    console.log(currentUserId);
+  } else {
+    currentUserId = req.body.user;
+
+    res.redirect("/");
+  }
 });
- 
-app.post("/new", async (req, res) => {
+
+app.post("/new",  (req, res) => {
   const name = req.body.name;
   const color = req.body.color;
 
-  const result = await db.query(
+  const result = db.query(
     "INSERT INTO users (name, color) VALUES($1, $2) RETURNING *;",
     [name, color]
   );
